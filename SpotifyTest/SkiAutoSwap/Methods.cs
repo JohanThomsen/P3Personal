d8ca167dd68712c9aa9d11b277919de02b10de4c
DESKTOP-M74EIJ9\johan <jthoms19@student.aau.dk>
2020-10-23 08:08:03 +0200

    skiswap forsøg :/

---
+++ SpotifyTest/SkiAutoSwap/Methods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+public class Methods
+{
+
+    private static readonly HttpClient client = new HttpClient();
+    public async void Test()
+    {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "BQCBcW0ZHStG0fj_-eMdDMZTyQYC0PRHmFk7DEFm5c_QVoU90UHRgZpPvVv_kmAhfz7AdmN7AjWDTzbGkbQBLTLWfaK4ftXckWTNAyMT1W2j3OvMSpohYoyyt1NM67JJwaF6_TyCVVYDXQZqa5RoO2IpVuHneQ13Y7RqH_8KY5Zm9YiexU10k8oaXILzhG_tGzt2Nzrb9GQ-UptuJNxWAoWPyDjFMb_Q9CrMRVzMzPTA2oQ_nuzMDtmY8_Z1EsODX92yLKUmX0WNdagV6vU");
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+        //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+        var values = new Dictionary<string, string>
+        {
+            { "Accept", "application/json" },
+            { "Content-Type", "application/json" },
+            { "Authorization", "Bearer, BQCBcW0ZHStG0fj_-eMdDMZTyQYC0PRHmFk7DEFm5c_QVoU90UHRgZpPvVv_kmAhfz7AdmN7AjWDTzbGkbQBLTLWfaK4ftXckWTNAyMT1W2j3OvMSpohYoyyt1NM67JJwaF6_TyCVVYDXQZqa5RoO2IpVuHneQ13Y7RqH_8KY5Zm9YiexU10k8oaXILzhG_tGzt2Nzrb9GQ-UptuJNxWAoWPyDjFMb_Q9CrMRVzMzPTA2oQ_nuzMDtmY8_Z1EsODX92yLKUmX0WNdagV6vU" }
+        };
+        var content = new FormUrlEncodedContent(values);
+
+        var response = await client.PutAsync("https://api.spotify.com/v1/me/player/pause", content);
+
+
+        Console.WriteLine("Here");
+        var responseString = await response.Content.ReadAsStringAsync();
+
+
+
+
+
+
+    }
+
+    public async 
+    Task Idk(string response)
+    {
+        Console.WriteLine(response);
+    }
+
+}
---
+++ SpotifyTest/SkiAutoSwap/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+
+namespace SkiAutoSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //1. Connect to Spotify
+            //2. Check if person is Going Upwards
+            //3. Figure out how to change to either Chill or banging Music Depending on going up or down
+            /*OAUTH Token BQCj3DoV05HORAY7KedCRrAdYmtd8v8VdeZcdJPOJoB2H9BuPSpXl5Ejjk0fVe8VdJ8DE3kVJOTQta9K1
+             * Chill playlistID: 77ENCz65prp4SfiZorFaK6
+             * Banging Playlist: 392z7ojTQVvPiMxDAG9Kqm
+             * Device ID PC : 163dcdf5287087c7842f07b3b77cfef23943de62
+             * Device ID Mobile: 4bfabfb5156e9a18c6239c09f9248e8278e3a43c */
+
+            Methods test = new Methods();
+
+            test.Test();
+
+            
+
+        }
+    }
+}
