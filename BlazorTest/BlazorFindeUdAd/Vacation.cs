c030d0cab6f7b8fed6d8648fc6edc8bdf557f374
jthoms19 <jthoms19@student.aau.dk>
2020-10-27 13:16:50 +0100

    så mads ikke ødelææger alt

--- BlazorTest/BlazorFindeUdAd/Vacation.cs
+++ BlazorTest/BlazorFindeUdAd/Vacation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BlazorFindeUdAd.Pages
+namespace BlazorFindeUdAd
 {
     public class Vacation
     {
---
+++ BlazorTest/TravelClubProto/Data/VacationList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelClubProto.Data
+{
+    public class VacationList
+    {
+        public int Age = 10;
+        public List<Vacation> PublishedVacations { get; }
+
+        public void PublishVacation(Vacation newVac)
+        {
+            PublishedVacations.Add(newVac);
+        }
+    }
+}
