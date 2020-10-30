634eb9461e37413bba69c78e1400eec411b6ec48
DESKTOP-M74EIJ9\johan <jthoms19@student.aau.dk>
2020-10-30 13:46:21 +0100

    Kuirse 7 Delegates

---
+++ OOP/Kurse7Delegates/DelegatesOpgaver/Methods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesOpgaver
+{
+    public delegate string StringJoin(string l, string r);
+    public delegate T Join<T>(T Tin1, T Tin2);
+    class Methods
+    {
+        public string JoinThree(string str1, string str2, string str3, StringJoin join)
+        { 
+            string newString;
+
+            newString = join(str1, str2);
+            newString = join(newString, str3);
+
+            return newString;
+        }
+
+        public string JoinAllStrings(List<string> strings, StringJoin join)
+        {
+            string newString = strings[0];
+            for (int i = 1; i <= strings.Count - 1; i++)
+            {
+                newString = join(newString, strings[i]);
+            }
+            return newString;
+        }
+
+        public T JoinAll<T>(List<T> items, Join<T> join)
+        {
+            T combined = items[0];
+
+            for (int i = 1; i <= items.Count - 1; i++)
+            {
+                combined = join(combined, items[i]);
+            }
+
+            return combined;
+        }
+
+        public bool Exists<T>(Predicate<T> pred, T[] arr)
+        {
+            bool result = false;
+            foreach (T item in arr)
+            {
+                if (pred(item))
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ OOP/Kurse7Delegates/DelegatesOpgaver/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegatesOpgaver
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Methods m = new Methods();
+
+            //StringJoin join = (l, r) => { return l + r; };
+            //string combined = join("Hello ", "Delegates");
+            //Console.WriteLine("First combine" + combined);
+
+            //string secondCombineNormal = m.JoinThree("a", "b", "c", (l, r) => l + r);
+            //Console.WriteLine(secondCombineNormal);
+            //string secondCombineDot = m.JoinThree("a", "b", "c", (l, r) => l + "." + r);
+            //Console.WriteLine(secondCombineDot);
+
+            //string joinAllCombine = m.JoinAllStrings(new List<string> { "a", "b", "c", "d" }, (l, r) => l + r);
+            //Console.WriteLine(joinAllCombine);
+            //string joinAllCombineDot = m.JoinAllStrings(new List<string> { "a", "b", "c", "d" }, (l, r) => l + "." + r);
+            //Console.WriteLine(joinAllCombineDot);
+
+            //Console.WriteLine(m.JoinAll(new List<int> { 1, 2, 3, 4 }, (a, b) => a + b));
+            //Console.WriteLine(m.JoinAll(new List<int> { 1, 2, 3, 4 }, (a, b) => a * b));
+            //Console.WriteLine(m.JoinAll(new List<string> { "a", "b", "c", "d" }, (l, r) => l + "." + r));
+
+            //bool checkFalse = m.Exists((item) => item == 2, new int[] { 1, 3, 5, 7, 9 });
+            //bool checkTrue = m.Exists((item) => item == 2, new int[] { 1, 2, 5, 7, 9 });
+            //Console.WriteLine($"Should be false {checkFalse}");
+            //Console.WriteLine($"Should be true {checkTrue}");
+            //checkTrue = m.Exists((item) => item == "Hello", new string[] { "Hello", "good" });
+            //checkFalse = m.Exists((item) => item == "Hello", new string[] { "No", "good" });
+            //Console.WriteLine($"Should be false {checkFalse}");
+            //Console.WriteLine($"Should be true {checkTrue}");
+        }
+    }
+}
